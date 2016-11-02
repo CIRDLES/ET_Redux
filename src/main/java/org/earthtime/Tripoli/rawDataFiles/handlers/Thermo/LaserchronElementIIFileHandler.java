2d3a5b060d042dfdde7917eb3805de5aa50a3e69
bowring <bowring@gmail.com>
2016-11-02 07:20:34 -0400

    fixes bug in livedata ref material counting and reporting

--- src/main/java/org/earthtime/Tripoli/rawDataFiles/handlers/Thermo/LaserchronElementIIFileHandler.java
+++ src/main/java/org/earthtime/Tripoli/rawDataFiles/handlers/Thermo/LaserchronElementIIFileHandler.java
@@ -118,9 +118,11 @@ public class LaserchronElementIIFileHandler extends AbstractRawDataFileHandler {
     @Override
     public void getAndLoadRawIntensityDataFile(SwingWorker loadDataTask, boolean usingFullPropagation, int leftShadeCount, int ignoreFirstFractions, boolean inLiveMode) {
 
-        referenceMaterialIncrementerMap = new ConcurrentHashMap<>();
-        for (int i = 0; i < rawDataFileTemplate.getStandardIDs().length; i++) {
-            referenceMaterialIncrementerMap.put(rawDataFileTemplate.getStandardIDs()[i], 1);
+        if ((referenceMaterialIncrementerMap == null) || !inLiveMode) {
+            referenceMaterialIncrementerMap = new ConcurrentHashMap<>();
+            for (String standardID : rawDataFileTemplate.getStandardIDs()) {
+                referenceMaterialIncrementerMap.put(standardID, 1);
+            }
         }
 
         // Laserchron ElementII has folder of .dat files 
